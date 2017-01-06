9851748a3f0bc9ad57f36c26d9223596da56dc82
binnng <binnng@163.com>
2017-01-06 23:37:54 +0800

    Site updated: 2017-01-06 23:37:54

--- js/app.js
+++ js/app.js
@@ -46,4 +46,14 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 
+var duoshuoQuery = {short_name:"mobike"};
+(function() {
+  var ds = document.createElement('script');
+  ds.type = 'text/javascript';ds.async = true;
+  ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+  ds.charset = 'UTF-8';
+  (document.getElementsByTagName('head')[0] 
+   || document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
+
 appDaily.bindToggleButton();
